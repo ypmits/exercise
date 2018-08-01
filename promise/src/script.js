4b06d1df6e4021d51349e0068e1880cc69d7b3d9
Mient-jan Stelling <mientjan.stelling@gmail.om>
2018-08-01 12:25:16 +0200

    adding urls

--- promise/src/script.js
+++ promise/src/script.js
@@ -3,19 +3,19 @@
  * @param url
  * @return {Promise<any>}
  */
-function loadImage(url){
-	return new Promise(function(resolve, reject){
-		var img = document.createElement('img');
-		img.onload = function(){
-			resolve(this);
-		}
+function loadImage(url) {
+  return new Promise(function(resolve, reject) {
+    var img = document.createElement("img");
+    img.onload = function() {
+      resolve(this);
+    };
 
-		img.onerror = function(e){
-			reject(e);
-		}
+    img.onerror = function(e) {
+      reject(e);
+    };
 
-		img.src = url;
-	})
+    img.src = url;
+  });
 }
 
 /**
@@ -26,11 +26,38 @@ function loadImage(url){
  * @param {number} y
  * @return {Promise<any>}
  */
-function animate(element, duration, x, y){
-	return new Promise(function(resolve){
-		TweenLite.to(element, duration, {x:x, y:y, onComplete: resolve})
-	})
+function animate(element, duration, x, y) {
+  return new Promise(function(resolve) {
+    TweenLite.to(element, duration, { x: x, y: y, onComplete: resolve });
+  });
 }
 
+var images = [
+  "./assets/001-yawn.png",
+  "./assets/002-wink.png",
+  "./assets/003-smile-1.png",
+  "./assets/004-smile.png",
+  "./assets/005-surprise.png",
+  "./assets/006-shocked.png",
+  "./assets/007-sceptic.png",
+  "./assets/008-sad-2.png",
+  "./assets/009-sad-1.png",
+  "./assets/010-happy-3.png",
+  "./assets/011-pain.png",
+  "./assets/012-muted.png",
+  "./assets/013-meh.png",
+  "./assets/014-laugh.png",
+  "./assets/015-ill.png",
+  "./assets/016-happy-2.png",
+  "./assets/017-happy-1.png",
+  "./assets/018-cute.png",
+  "./assets/019-crying.png",
+  "./assets/020-crazy.png",
+  "./assets/021-cool.png",
+  "./assets/022-bored.png",
+  "./assets/023-blush.png",
+  "./assets/024-sad.png",
+  "./assets/025-happy.png"
+];
 
 /// WRITE CODE UNDER HERE
